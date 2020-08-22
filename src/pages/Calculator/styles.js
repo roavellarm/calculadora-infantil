98e83586274c00f28e3055ed4c554c78842b2a11
Rodrigo Avellar <roavellarm@gmail.com>
2020-08-22 07:43:32 -0300

    Update styles

--- src/pages/Calculator/styles.js
+++ src/pages/Calculator/styles.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
   color: #fff;
   display: flex;
   width: 100%;
-  max-width: 500px;
+  max-width: 600px;
 
   flex-direction: column;
   justify-content: center;
@@ -17,19 +17,68 @@ export const Container = styled.div`
 `
 
 export const Screen = styled.div`
+  width: 100%;
+  height: 30rem;
+  padding: 2rem;
+  margin: 30px;
+
   background-color: ${transparentize(0.5, 'white')};
+  border-radius: 0.8rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`
+
+export const Message = styled.div`
+  display: flex;
+  justify-content: center;
+  color: #333;
+  height: 8rem;
+  font-size: 3rem;
+  letter-spacing: 0px;
+  font-weight: 300;
+  padding-bottom: 2rem;
+`
+
+export const Numbers = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  background: lightcoral;
+  height: 8rem;
   color: #333;
   font-weight: 900;
   font-size: 4rem;
-  border-radius: 0.8rem;
+  letter-spacing: ${({ hasOperator }) => hasOperator && '3rem'};
+`
+
+export const Result = styled.div`
   display: flex;
-  height: 10rem;
-  letter-spacing: 1rem;
-  justify-content: flex-end;
   align-items: center;
-  padding: 2rem;
-  margin: 30px;
-  width: 100%;
+  justify-content: center;
+  color: #333;
+  background: yellow;
+  padding: 1rem;
+  border-radius: 100px;
+  font-weight: 900;
+  font-size: 4rem;
+  height: 6rem;
+  width: 6rem;
+  letter-spacing: 0px;
+`
+
+export const Letters = styled.div`
+  display: flex;
+  justify-content: center;
+  color: #333;
+  height: 8rem;
+  font-size: 3rem;
+  letter-spacing: 0px;
+  font-weight: 300;
+  padding-bottom: 2rem;
 `
 
 export const Title = styled.h1`
@@ -44,19 +93,23 @@ export const Row = styled.div`
 `
 
 export const Button = styled.button`
-  background: ${({ disabled, value }) => {
+  background: ${({ disabled, type }) => {
     if (disabled) return 'grey'
-    return isNaN(value) ? 'lightblue' : '#283d3b'
+    if (type === 'number') return '#283d3b'
+    if (type === 'operator') return 'lightblue'
+    if (type === 'util') return 'red'
+    return 'green'
   }};
   width: 100%;
   margin: 0.5rem;
   padding: 1.5rem;
   border-width: 0px;
   border-radius: 2rem;
-  color: #fff;
+  color: ${({ disabled }) => (disabled ? 'lightgrey' : '#fff')};
   font-size: 3rem;
   font-weight: 900;
   outline: 0px;
+  opacity: ${({ disabled }) => disabled && '0.5'};
 
   :hover {
     opacity: ${({ disabled }) => disabled || '0.6'};
