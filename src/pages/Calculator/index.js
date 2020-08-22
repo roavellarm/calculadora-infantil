51295b0f9014593d4f46405a5c7ee26393f176de
Rodrigo Avellar <roavellarm@gmail.com>
2020-08-22 03:53:22 -0300

    Update calculator

--- src/pages/Calculator/index.js
+++ src/pages/Calculator/index.js
@@ -1,96 +1,67 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 // import porExtenso from 'por-extenso'
+import buttons from './buttonsInformation'
 import { Container, Title, Screen, Row, Button } from './styles'
 
-const buttons = [
-  [{ value: 7 }, { value: 8 }, { value: 9 }, { value: '/' }],
-  [{ value: 4 }, { value: 5 }, { value: 6 }, { value: 'X' }],
-  [{ value: 1 }, { value: 2 }, { value: 3 }, { value: '-' }],
-  [{ value: 0 }, { value: '=' }, { value: 'Limpa' }, { value: '+' }],
-]
-
 export default function Calculator() {
-  const disableButtonInitialState = ['=', 'Limpa', '+', '-', 'X', '/']
+  const disableInitialState = ['operator', 'equal', 'util']
+  const [screen, setScreen] = useState('')
+  const [disable, setDisable] = useState(disableInitialState)
 
-  const [screen, setScreen] = useState([])
-  const [firstNum, setFirstNum] = useState('')
-  const [operator, setOperator] = useState('')
-  const [secondNum, setSecondNum] = useState('')
-  const [disableButton, setDisableButton] = useState(disableButtonInitialState)
-
-  function handleNumbers(num) {
-    if (operator.length) {
-      setSecondNum([...secondNum, num])
-    }
-    setFirstNum([...firstNum, num])
-    return setScreen([...screen, num])
+  const backToInitialState = () => {
+    setDisable(disableInitialState)
+    setScreen([])
+  }
+  const disableType = type => setDisable(dis => [...dis, type])
+  const enableType = type => {
+    return setDisable(dis => dis.filter(item => item !== type))
   }
 
-  function cleanScreen() {
-    setDisableButton(disableButtonInitialState)
-    setFirstNum('')
-    setSecondNum('')
-    setOperator('')
-  }
+  const addToScreen = value => setScreen([...screen, value])
 
-  function handleLetters(letter) {
-    if (letter === 'Limpa') {
-      return cleanScreen()
-    }
-
-    if (letter === '=') {
-      if (secondNum.length) {
-        return alert('executar a operação')
-      } else {
-        if (operator.length) {
-          return alert('Coloque o segundo número')
-        } else {
-          return alert('Selecione algum operador: + - / X')
-        }
-      }
-    }
-
-    return setOperator(letter)
-  }
-
-  function handleDigitation(e) {
+  const handleKeyboard = e => {
     const { value } = e.target
-    return isNaN(value) ? handleLetters(value) : handleNumbers(value)
-  }
+    if (value === 'Limpa') return backToInitialState()
 
-  useEffect(() => {
-    if (firstNum.length && disableButton.includes('Limpa')) {
-      const newDisableArray = disableButton.filter((value) => {
-        return value === '='
-      })
-      return setDisableButton(newDisableArray)
+    if (!screen.length) {
+      disableType('number')
+      enableType('operator')
+      enableType('util')
+      return addToScreen(value)
     }
-  }, [firstNum, disableButton, operator])
 
-  useEffect(() => {
-    setScreen([firstNum, operator, secondNum])
-  }, [firstNum, operator, secondNum])
+    if (screen.length === 1) {
+      disableType('operator')
+      enableType('number')
+      return addToScreen(value)
+    }
+    if (screen.length === 2) {
+      disableType('number')
+      enableType('equal')
+      return addToScreen(value)
+    }
+    if (screen.length === 3) {
+      return addToScreen(value)
+    }
+    return null
+  }
 
   return (
     <Container>
       <Title>Calculadora</Title>
 
-      <Screen>
-        {screen.map((char) => {
-          return <>{char}</>
-        })}
-      </Screen>
+      <Screen>{screen}</Screen>
 
-      {buttons.map((row) => {
+      {buttons.map(row => {
         return (
           <Row key={row[0].value}>
-            {row.map((button) => {
+            {row.map(button => {
               return (
                 <Button
                   key={button.value}
                   value={button.value}
-                  onClick={(e) => handleDigitation(e)}
-                  disableButtond={disableButton.includes(button.value)}
+                  onClick={e => handleKeyboard(e)}
+                  disabled={disable.includes(button.type)}
                 >
                   {button.value}
                 </Button>
