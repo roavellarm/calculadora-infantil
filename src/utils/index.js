3d23eb6fa12676b99a8a57500171d7f25003bf98
Rodrigo Avellar <roavellarm@gmail.com>
2020-08-22 07:43:01 -0300

    Add Capitalize and fix operators at utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,11 @@
-export const sum = (num1, num2) => num1 + num2
+export const sum = (num1, num2) => (Number(num1) + Number(num2)).toString()
 
-export const mult = (num1, num2) => num1 * num2
+export const mult = (num1, num2) => (Number(num1) * Number(num2)).toString()
 
-export const min = (num1, num2) => num1 - num2
+export const min = (num1, num2) => (Number(num1) - Number(num2)).toString()
 
-export const div = (num1, num2) => num1 / num2
+export const div = (num1, num2) => (Number(num1) / Number(num2)).toString()
+
+export const capitalize = s => {
+  return typeof s !== 'string' ? '' : s.charAt(0).toUpperCase() + s.slice(1)
+}
